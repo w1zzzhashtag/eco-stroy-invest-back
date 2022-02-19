87fee703ee35e0ef8337bb96c77791f016c52405
v.kuzmin <vladislav.kuzmin@nordclan.com>
2022-02-19 19:47:41 +0400

    Заменил почту

--- index.js
+++ index.js
@@ -6,62 +6,62 @@ require("dotenv").config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 const PORT = process.env.PORT || 5000;
 
 const transporter = nodemailer.createTransport({
-  port: 465,
-  host: "smtp.yandex.ru",
-  auth: {
-    user: "info@экостроинвест.рф",
-    pass: process.env.TRANSPORTER_AUTH_PASSWORD,
-  },
-  secure: true,
+    port: 465,
+    host: "smtp.yandex.ru",
+    auth: {
+        user: "info@ecostroinvest.ru",
+        pass: process.env.TRANSPORTER_AUTH_PASSWORD,
+    },
+    secure: true,
 });
 
 app.post("/email", (req, res) => {
-  const { subject, name, phone, dateTime } = req.body;
-  if (!subject || !name || !phone) {
-    res.status(400).send({ message: `Сервер не может обработать этот запрос` });
-  }
+    const {subject, name, phone, dateTime} = req.body;
+    if (!subject || !name || !phone) {
+        res.status(400).send({message: `Сервер не может обработать этот запрос`});
+    }
 
-  const parseDate = (dateISO) => {
-    const date = new Date(dateISO);
-    const day = date.getDate();
-    const month = date.getMonth() + 1;
-    const year = date.getFullYear();
-    const hours = date.getHours();
-    const minutes = date.getMinutes();
-    const seconds = date.getSeconds();
-    return `${day <= 9 ? "0" + day : day}.${
-      month <= 9 ? "0" + month : month
-    }.${year} ${hours}:${minutes}:${seconds}`;
-  };
+    const parseDate = (dateISO) => {
+        const date = new Date(dateISO);
+        const day = date.getDate();
+        const month = date.getMonth() + 1;
+        const year = date.getFullYear();
+        const hours = date.getHours();
+        const minutes = date.getMinutes();
+        const seconds = date.getSeconds();
+        return `${day <= 9 ? "0" + day : day}.${
+            month <= 9 ? "0" + month : month
+        }.${year} ${hours}:${minutes}:${seconds}`;
+    };
 
-  const mailData = {
-    from: "info@экостроинвест.рф",
-    to: "info@экостроинвест.рф",
-    subject: "ЭкоСтройИнвест",
-    html: `
+    const mailData = {
+        from: "info@ecostroinvest.ru",
+        to: "info@ecostroinvest.ru",
+        subject: "ЭкоСтройИнвест",
+        html: `
         <div><b>Тема: </b> ${subject}</div>
         <div><b>Имя: </b> ${name}</div>
         <div><b>Телефон: </b> ${phone}<div />
         <div><b>Дата и время: </b> ${
-          dateTime ? parseDate(dateTime) : "не указано"
+            dateTime ? parseDate(dateTime) : "не указано"
         }</div>
     `,
-  };
+    };
 
-  transporter.sendMail(mailData, function (err, info) {
-    if (err) {
-      console.log(err);
-      res.status(500).send({ message: "Ну удалось отправить сообщение" });
-    } else {
-      res.status(200).send({ message: "Mail send", messageId: info.messageId });
-    }
-  });
+    transporter.sendMail(mailData, function (err, info) {
+        if (err) {
+            console.log(err);
+            res.status(500).send({message: "Ну удалось отправить сообщение"});
+        } else {
+            res.status(200).send({message: "Mail send", messageId: info.messageId});
+        }
+    });
 });
 
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+    console.log(`Server listening on port ${PORT}`);
 });
